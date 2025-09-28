926dc1d2e4ff54edcfddb6ea4a1558256e13203b
sameerkindia <sameerkhanaa5@gmail.com>
2025-09-28 11:40:55 +0530

    app tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { SafeAreaView, SafeAreaViewBase, StyleSheet, Text, View } from "react-native";
 import QuizScreen from "./src/app/QuizScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import QuizProvider from "./src/context/QuizProvider";
 
 export default function App() {
   return (
     <SafeAreaProvider style={{ flex: 1 }}>
+      <QuizProvider>
       <QuizScreen />
+      </QuizProvider>
     </SafeAreaProvider>
   );
 }
