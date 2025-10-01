6ee2a88f1b84ffc3b960dd6364f672d576d65df9
sameerkindia <sameerkhanaa5@gmail.com>
2025-10-01 08:52:46 +0530

    Quiz provider updated

--- src/context/QuizProvider.tsx
+++ src/context/QuizProvider.tsx
@@ -4,16 +4,19 @@ import {
   PropsWithChildren,
   SetStateAction,
   useContext,
+  useEffect,
   useState,
 } from "react";
 import questions from "../questions";
 import { Question } from "../types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type QuizContext = {
   question?: Question;
   questionIndex: number;
   selectedOption?: string;
   score: number;
+  bestScore: number;
   totalQuestions: number;
   setSelectedOption: Dispatch<SetStateAction<string | undefined>>;
   // setSelectedOption: () => void;
@@ -24,6 +27,7 @@ export const QuizContext = createContext<QuizContext>({
   questionIndex: 0,
   score: 0,
   totalQuestions: 0,
+  bestScore: 0,
   // selectedOption: '',
   setSelectedOption: () => {},
   onNext: () => {},
@@ -33,9 +37,22 @@ export default function QuizProvider({ children }: PropsWithChildren) {
   const [questionIndex, setQuestionIndex] = useState(0);
   const [selectedOption, setSelectedOption] = useState<string | undefined>();
   const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
-  const isFinished = questionIndex > questions.length;
+  const isFinished = questionIndex >= questions.length;
+  // console.log(isFinished , questionIndex , questions.length)
   const question = questions[questionIndex];
+
+  useEffect(() => {
+    loadingBestScore();
+  }, []);
+
+  useEffect(() => {
+    if (isFinished === true && score > bestScore) {
+      setBestScore(score);
+      saveBestScore(score);
+    }
+  }, [isFinished]);
 
   const restart = () => {
     setQuestionIndex(0);
@@ -56,6 +73,26 @@ export default function QuizProvider({ children }: PropsWithChildren) {
     setQuestionIndex((currQuestion) => currQuestion + 1);
   };
 
+  const saveBestScore = async (value: number) => {
+    try {
+      // console.log("Save best score: ", value);
+      await AsyncStorage.setItem("best-score", value.toString());
+    } catch (e) {
+      // saving error
+    }
+  };
+
+  const loadingBestScore = async () => {
+    try {
+      const value = await AsyncStorage.getItem("best-score");
+      if (value !== null) {
+        setBestScore(Number.parseInt(value));
+      }
+    } catch (e) {
+      // error reading value
+    }
+  };
+
   // console.log(score, " this is score");
 
   return (
@@ -68,6 +105,7 @@ export default function QuizProvider({ children }: PropsWithChildren) {
         setSelectedOption,
         score,
         totalQuestions: questions.length,
+        bestScore,
       }}
     >
       {children}
